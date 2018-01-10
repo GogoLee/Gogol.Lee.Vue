12ca853472a5d13304478736405b3a83783a2ea3
lxg.net.mvc <lxg.net.mvc@hotmail.com>
2018-01-10 10:46:11 +0800

    Gogol.Lee
    Signed-off-by: lxg.net.mvc <lxg.net.mvc@hotmail.com>

--- src/components/view/shared/sidebar.vue
+++ src/components/view/shared/sidebar.vue
@@ -1,35 +1,30 @@
 <<template>
-  <el-menu
-      default-active="2"
-      
-      @open="handleOpen"
-      @close="handleClose">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
+  <el-menu :default-active="onRoutes" class="el-menu--dark" background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b" router>
+      <template v-for="item in items">
+        <template v-if="item.subs">
+          <el-submenu :index="item.index">
+            <template slot="title"><i :class="item.icon"></i>{{ item.title }}</template>
+            
+            <el-menu-item-group v-for="(sub,i) in item.subs" :key="i" :index="sub.index">
+              <template v-if="sub.subs">
+                <el-submenu :index="sub.index">
+                  <template slot="title">{{ sub.title }}</template>
+                  <el-menu-item v-for="(subItem,i) in sub.subs" :key="i" :index="subItem.index">{{ subItem.title }}</el-menu-item>
+                </el-submenu>
+              </template>
+              <template v-else>
+                <el-menu-item :index="sub.index">{{ sub.title }}</el-menu-item>
+              </template>
+            </el-menu-item-group>
+          </el-submenu>
         </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
+        <template v-else>
+          <el-menu-item :index="item.index"><i :class="item.icon"></i>{{ item.title }}</el-menu-item>
+        </template>
+      </template>
+      
     </el-menu>
 </template>
 
@@ -37,8 +32,56 @@
 export default {
   data() {
     return {
-      isCollapse: true
+      isCollapse: true,
+      items: [
+        {
+          icon: "el-icon-menu",
+          index: "home",
+          title: "主页"
+        },
+        {
+          icon: "el-icon-menu",
+          index: "2",
+          title: "人事管理",
+          subs: [
+            {
+              index: "2-1",
+              title: "用户管理",
+              subs: [
+                {
+                  index: "perssion",
+                  title: "权限管理"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          icon: "el-icon-document",
+          index: "3",
+          title: "表单",
+          subs: [
+            {
+              index: "form",
+              title: "基本表单"
+            },
+            {
+              index: "form_validate",
+              title: "表单验证"
+            },
+            {
+              index: "table",
+              title: "表格"
+            }
+          ]
+        }
+      ]
     };
+  },
+  computed: {
+    onRoutes() {
+      return this.$route.path.replace("/", "");
+    }
   },
   methods: {
     handleOpen(key, keyPath) {
